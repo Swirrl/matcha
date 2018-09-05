a4dd75fae9111b138f84ded5eec596cc23efe8da
Rick Moynihan <rick.m@swirrl.com>
2018-09-05 11:41:36 +0100

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject grafter/matcha.alpha "0.1.0-SNAPSHOT"
+(defproject grafter/matcha.alpha "0.1.0"
   :description "A SPARQL-like query engine and DSL for querying in
   memory RDF models."
   :url "https://github.com/Swirrl/matcha"
