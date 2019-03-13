8afd93d65798372cd7a2136ac6407eda77551b11
Rick Moynihan <rick.m@swirrl.com>
2019-03-13 11:28:33 +0000

    Perf test suite tweaks
    Mainly removing perf tests that are no longer that enlightening.

--- test/grafter/matcha/perf_test.clj
+++ test/grafter/matcha/perf_test.clj
@@ -34,13 +34,14 @@
                       parsed))))
 
 (defmacro def-perf-suite [name & forms]
-  `(deftest  ~(with-meta name {:perf true})
+  `(deftest  ~(with-meta name (merge {:perf true}
+                                     (meta name)))
      ~@forms
      (is true)))
 
 (def fully-indexed [:s :p :o [:s :p] [:s :o] [:p :o]])
 
-(def-perf-suite index-triples-test
+(def-perf-suite ^:indexer index-triples-test
   (let [quads (into #{} (rdf/statements (io/resource "zib/column-db/column-db-xl-1.nt")))]
     (report-bench
      (mdb/index-triples [:s] quads)
@@ -59,7 +60,7 @@
   ;; XXL is the same as XL but with all the SKOS parent relationships loaded too,
   ;; from the HMRC overseas trade dataset: http://gss-data.org.uk/data/hmrc-overseas-trade-statistics
 
-  (def-perf-suite index-triples-test
+  (def-perf-suite ^:indexer index-triples-test
     (let [quads (into #{} (rdf/statements (io/resource "zib/column-db/column-db-xxl-1.nt")))]
       (report-bench
        (mdb/index-triples [:s] quads)                ;; 1.88 secs
@@ -69,9 +70,10 @@
        (mdb/index-triples [:s :p :o [:s :p]] quads)  ;; 6.50 secs
        (mdb/index-triples [:s :p :o [:s :p] [:s :o]] quads) ;; 8.34 secs
        (mdb/index-triples fully-indexed quads)       ;; 10.57 secs
-       )))
+       ))))
 
-  )
+
+
 
 (defn construct-by-type
   "A common matcha query function from zib codebase."
@@ -89,7 +91,7 @@
 (def zib:MeasureColumn (zib "MeasureColumn"))
 (def zib:hasCodeList (zib "hasCodeList"))
 
-(def-perf-suite zib-column-db-constructs
+(def-perf-suite ^:indexes zib-column-db-constructs
   (let [quads (into #{} (rdf/statements (io/resource "zib/column-db/column-db-xl-1.nt")))]
     (println "Running zib-column-db-constructs with different indexes on" (count quads) "triples")
     (let [matcha-spo (mdb/index-triples [:s :p :o] quads)
@@ -132,27 +134,6 @@
 
 (def comp-with-most-dimvals (URI. "http://gss-data.org.uk/data/gss_data/trade/hmrc_ots_cn8/component/combined_nomenclature"))
 
-(defn find-top-concepts [column-db]
-  (let [concept-schemes (distinct (m/construct ?concept_scheme
-                                                    [[comp-with-most-dimvals zib:hasCodeList ?codelist]
-                                                     [?codelist skos:member ?code]
-                                                     [?code skos:inScheme ?concept_scheme]] column-db))
-        top-concepts (mapcat (fn [concept-scheme]
-                               (m/construct {:grafter.rdf/uri ?top
-                                             ?p ?o}
-                                            [[?top skos:topConceptOf concept-scheme]
-                                             [?top ?p ?o]] column-db)) concept-schemes)
-
-        ;; query-flat-codes (m/construct {:grafter.rdf/uri ?code
-        ;;                                ?p ?o}
-        ;;                               [[comp-with-most-dimvals zib:hasCodeList ?codelist]
-        ;;                                [?codelist skos:member ?code]
-        ;;                                [?code ?p ?o]])
-        ]
-
-    (count top-concepts)))
-
-
 (defn find-top-concepts-values [column-db]
   (let [concept-schemes (distinct (m/construct ?concept_scheme
                                                [[comp-with-most-dimvals zib:hasCodeList ?codelist]
@@ -173,25 +154,7 @@
 
       (report-bench
        (find-top-concepts-values matcha-spo) ;; 3.88 secs (old) 1.9 secs (new)
-       ;;(find-top-concepts matcha-spo)        ;; 3.98 secs
-
-
-       ;; Above results show that values clauses are as fast as
-       ;; injecting them yourself
-
-       ;;(find-top-concepts-one-query matcha-spo)
-
        ))))
-
-(def-perf-suite zib-skos
-  (let [quads (into #{} (rdf/statements (io/resource "zib/column-db/column-db-xxl-1.nt")))]
-    (println "Running zib-column-db-constructs with different indexes on" (count quads) "triples")
-    (let [matcha-spo (mdb/index-triples [:s :p :o] quads)
-          matcha-sp (mdb/index-triples [[:s :p]] quads) ;; 9.03 µs
-          ]
-      (report-bench
-       (find-top-concepts matcha-sp)
-       (find-top-concepts matcha-spo)))))
 
 (def-perf-suite construct-vs-select
   (let [db-unindexed (into #{} (rdf/statements (io/resource "zib/column-db/column-db-xxl-1.nt")))
@@ -201,70 +164,29 @@
      (count (m/select
               [?narrower ?p ?o]
               [[?narrower rdf:a skos:Concept]
-               [?narrower ?p ?o]] db-unindexed))
-
-     (count (m/construct {:grafter.rdf/uri ?s
-                          ?p ?o}
-                         [[?narrower rdf:a skos:Concept]
-                          [?narrower ?p ?o]] db-unindexed))
-
-     ;; tests indexing query time on s p o
-     (count (m/select
-              [?narrower ?p ?o]
-              [[?narrower rdf:a skos:Concept]
                [?narrower ?p ?o]] db-spo))
 
-     (count (m/construct {:grafter.rdf/uri ?s
-                          ?p ?o}
-
+     ;; should only be marginally slower than the above select ...
+     (count (m/construct [?narrower ?p ?o]
                          [[?narrower rdf:a skos:Concept]
-                          [?narrower ?p ?o]] db-spo))
+                          [?narrower ?p ?o]] db-spo)))))
 
-     ;; test indexing [p o] + query time on [p o]
+(def-perf-suite compound-indexes
+  (let [db-unindexed (into #{} (rdf/statements (io/resource "zib/column-db/column-db-xxl-1.nt")))
+        db-po (mdb/index-triples [#{:p :o}] db-unindexed)]
+    (report-bench
      (count (m/select
               [?narrower ?p ?o]
               [[?narrower rdf:a skos:Concept]
-               [?narrower ?p ?o]] (mdb/index-triples [#{:p :o}] db-unindexed)))
+               [?narrower ?p ?o]] db-po)))))
 
-     (count (m/construct {:grafter.rdf/uri ?s
-                          ?p ?o}
-                         [[?narrower rdf:a skos:Concept]
-                          [?narrower ?p ?o]] (mdb/index-triples [#{:p :o}] db-unindexed))))))
-
-;; The tests below test how pldb (the native core.logic db indexer)
-;; indexes triples.  This is how matcha used to do it.
-;;
-;; Our approach is slightly faster, but more flexible as the indexes
-;; can be configured at runtime; allowing you to save time by not
-;; building unnecessary indexes.  Our approach also supports composite
-;; indexes not supported by pldb.
-
-
-(pldb/db-rel triple ^:index s ^:index p ^:index o)
-
-(defn triple-vector->idx-triple
-  "Assume triples are either 3/tuple vectors or can be destructured as
-  such.  Grafter Quad objects can be destructured in this manner."
-  [[s p o]]
-  [triple s p o])
-
-(defn old-index-triples
-  "Return an indexed database of triples. Indexing a database will
-  result in better performance if you want to run multiple queries
-  over the same database.
-  All query functions should accept either a sequence of triples or an
-  indexed database."
-  [db]
-  (with-meta (apply pldb/db (map triple-vector->idx-triple db))
-    {::index true}))
-
-
-(def-perf-suite old-index-triples-test
-  (let [quads (into #{} (rdf/statements (io/resource "zib/column-db/column-db-xl-1.nt")))]
+(def-perf-suite construct-resource-grouping
+  (let [db-unindexed (into #{} (rdf/statements (io/resource "zib/column-db/column-db-xxl-1.nt")))
+        db-spo (mdb/index-triples [:s :p :o] db-unindexed)]
     (report-bench
-     (old-index-triples quads))))
-
-
+     (count (m/construct [?narrower ?p ?o]
+                         [[?narrower rdf:a skos:Concept]
+                          [?narrower ?p ?o]] db-spo)))))
 
 (comment
 
