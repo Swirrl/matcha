38ff11b0872589a1ba48c416931db511c0ce5450
Rick Moynihan <rick.m@swirrl.com>
2019-09-13 14:11:07 +0100

    Remove cruft and implement the beginnings of a new query compiler
    This one will take a query as data, and compile at run time into a
    core.logic function by dynamically composing logic functions.
    
    Lots left to do, currently just projects all query bindings, and only
    supports conjunctive queries (BGPs).

--- src/grafter/matcha/alpha2.clj
+++ src/grafter/matcha/alpha2.clj
@@ -19,145 +19,76 @@
   [triple s p o])
 
 
-
-
-(def test-rel
-  (fn [& query]
-      (println "q" query)
-      (fn [& stuff]
-        (println stuff))))
-
 (def matcha-db (apply pldb/db (map triple-vector->idx-triple [[:rick :foaf/friend :katie]
                                                               [:katie :foaf/friend :julie]
                                                               [:julie :foaf/name "Julie"]])))
 
-(pldb/with-db matcha-db
-  (l/run 2 [?name]
-    (fresh [?s ?o ?o2]
-      (l/and*
-       [(triple ?s :foaf/friend ?o)
-        (triple ?o :foaf/friend ?o2)
-        (triple ?o2 :foaf/name ?name)]))))
-
-
-;; walking stuff
-(let [v (l/lvar)]
-  (l/walk* ((l/== v 1) l/empty-s) v))
-
-
-;; use solutions as a runtime variant of -run
-(let [v (l/lvar)
-
-      g (l/or* [(l/== v 1)
-                (l/== v 2)])]
-
-  (l/solutions l/empty-s v g))
-
-
-(defn build-query []
-  (let [?s (l/lvar "s")
-        ?o (l/lvar "o")
-        ?o2 (l/lvar "o2")
-        ?name (l/lvar "name")
-
-        t1 (apply triple [?s :foaf/friend ?o])
-        t2 (apply triple [?o :foaf/friend ?o2])
-        t3 (apply triple [?o2 :foaf/name ?name])]
-
-    {:goal [t1 t2 t3]
-     :bindings [?s ?o ?o2 ?name]}))
-
-;; NOTE this one works!!!
-(let [the-db (apply pldb/db [[triple :rick :foaf/friend :katie]])
-      {:keys [bindings goal]} (build-query)
-      results (l/lvar "results")
-
-      ultimate-g #_(l/and* (conj goal (l/== results bindings))) (apply triple [results :foaf/friend :katie])
-
-      ]
-  (l/solutions (l/tabled-s true {:db [the-db]
-                                 :reify-vars true})
-               results
-               ultimate-g))
-
-
-;; THIS IS IT!!!!! THE ONE THAT WORKS!!!!
-(let [the-db matcha-db #_(apply pldb/db [[triple :rick :foaf/friend :katie]])
-      {:keys [bindings goal]} (build-query)
-      results (l/lvar "results")
-
-      ultimate-g (l/and* (conj goal (l/== results bindings)))
-
-
-      ]
-  (l/solutions (l/tabled-s true {:db [the-db]
-                                 :reify-vars true})
-               results
-               ultimate-g))
-
-
-
-
-
-
-#_(let*
-    []
-    (push-thread-bindings
-     (hash-map #'l/*logic-dbs* (conj l/*logic-dbs* matcha-db)))
-    (try
-      (let*
-          [opts {:db l/*logic-dbs*, :n 2, :occurs-check true}
-           xs (lp/take* (fn* []
-                             ((fn* ([arg]
-                                    (fn*
-                                     -inc
-                                     ([]
-                                      (let* [?name (l/lvar '?name)]
-                                        (lp/bind (lp/bind
-                                                  arg
-                                                  (fn*
-                                                   ([arg]
-                                                    (fn*
-                                                     -inc
-                                                     ([]
-                                                      (let*
-                                                          [ ;?s (l/lvar '?s)
-                                                           ?o (l/lvar '?o)
-                                                           ?o2 (l/lvar '?o2)]
-                                                          (lp/bind arg
-                                                                   (l/and*
-                                                                    [(triple (l/lvar '?s) :foaf/friend ?o)
-                                                                     (triple ?o :foaf/friend ?o2)
-                                                                     (triple ?o2 :foaf/name ?name)]))))))))
-                                                 (l/reifyg ?name)))))))
-                              (l/tabled-s
-                               (:occurs-check opts)
-                               (merge {:reify-vars true} opts)))))]
-          (let*
-              [nsols (:n opts)]
-              (if nsols
-                (let* [n nsols]
-                  (take n xs ))
-                xs )))
-      (finally (pop-thread-bindings))))
-
-(defn substitute-vars [query-data]
+(defn- varify [query-data]
   (let [distinct-qvars (set (for [triple query-data
                                   term (filter symbol? triple)]
                               term))
-        sym->lvar (zipmap distinct-qvars (repeatedly l/lvar))]
+        sym->lvar (->> distinct-qvars
+                       (map (juxt identity (comp l/lvar str)))
+                       (into {}))]
 
-    (for [triple query-data]
-      (mapv (fn [term]
-             (sym->lvar term term)) triple))))
+    {::bindings sym->lvar
+     ::query (for [triple query-data]
+              (mapv (fn [term]
+                      (sym->lvar term term)) triple))}))
+
+(defn- goalify [{:grafter.matcha.alpha2/keys [query bindings] :as qdata}]
+  (let [goals (for [bgp query]
+                (apply triple bgp))
+
+        result-var (l/lvar "results")
+        compiled-goals (l/and* (conj goals
+                                     (l/== result-var bindings)))]
+
+    (assoc qdata
+           ::result-var result-var
+           ::compiled-goals compiled-goals)))
+
+(defn compile-query
+  "Compile a query"
+  [q]
+  (goalify (varify q)))
+
+(defn run-query* [matcha-db {:grafter.matcha.alpha2/keys [bindings compiled-goals result-var] :as compiled-query}]
+  (l/solutions (l/tabled-s true {:db [matcha-db]
+                                 :reify-vars false})
+               result-var
+               compiled-goals))
+
+(defn run-query
+  "Compile a query"
+  [matcha-db query]
+  (run-query* matcha-db (compile-query query)))
+
+(comment
+
+  (def foaf-query '[[?s :foaf/friend ?o]
+                    [?o :foaf/friend ?o2]
+                    [?o2 :foaf/name ?name]])
+
+  (varify foaf-query)
+  (goalify foaf-query)
 
 
-(def my-query (->> '[[?s :foaf/friend ?o]
-                     [?o :foaf/friend ?o2]
-                     [?o2 :foaf/name ?name]]
-                   substitute-vars
-                   (map (fn [[s p o]] [triple s p o]))))
+  (compile-query foaf-query)
+
+  (run-query matcha-db foaf-query))
 
 
 
-(def all-lvars (distinct (mapcat #(filter l/lvar? %) my-query)))
+
+
+
+;; (def my-query (->> '[[?s :foaf/friend ?o]
+;;                      [?o :foaf/friend ?o2]
+;;                      [?o2 :foaf/name ?name]]
+;;                    substitute-vars
+;;                    (map (fn [[s p o]] [triple s p o]))))
+
+
+
+;;(def all-lvars (distinct (mapcat #(filter l/lvar? %) my-query)))
