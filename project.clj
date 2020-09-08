bd95fc6620344f87e9d5b605364d809338568607
Rick Moynihan <rick.m@swirrl.com>
2020-09-08 09:21:39 +0100

    Upgrade core.logic to 1.0.0
    Main change is it removes the WARNING about indexed? being shadowed
    under clojure 1.10

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/core.logic "0.8.11"]]
+                 [org.clojure/core.logic "1.10.0"]]
 
   :profiles {:dev {:dependencies [[grafter "2.0.0"]]}}
 
